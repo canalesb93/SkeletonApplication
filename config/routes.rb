e601f8fa721f3ef0ace1a84b06d8e57c488a8b99
Ricardo Canales <canalesb93@gmail.com>
2014-08-10 20:36:27 -0500

    Real Routes root

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  get 'static/about'
-  get 'static/home'
-  get 'static/help'
 
-  resources :microposts
-
-  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static#home'
 
+  get 'static/about'
+  get 'static/home'
+  get 'static/help'
+  
+  resources :microposts
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
