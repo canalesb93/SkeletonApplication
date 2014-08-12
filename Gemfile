6dd5ef71bb924b874da482e79bc639b24423bd6a
Ricardo Canales <canalesb93@gmail.com>
2014-08-12 01:58:23 -0500

    Makes basic user model including password authentication, 1 failed test for unknown reason

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.4'
 gem 'bootstrap-sass', '~> 3.2.0'
+gem 'bcrypt-ruby'
 # Use sqlite3 as the database for Active Record
 group :development, :tests do
   gem 'sqlite3'
--- spec/requests/statics_spec.rb
+++ spec/requests/statics_spec.rb
@@ -4,30 +4,47 @@ RSpec.describe "Statics pages", :type => :request do
 
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
-
-    it { should have_content('Activity Page')}
-    it { should have_title('Canales')}
-    it { should_not have_title('| Home')}
-  end
-
-  describe "About page" do
-    before { visit about_path}
-
-    it { should have_title('About')}
-    it { should have_content('About me')}
-  end
-
-  describe "Contact page" do
-    it "should have the right title" do
-      visit contact_path
-      expect(page).to have_title("Contact")
+    shared_examples_for "all static pages" do
+      it { should have_selector('h1', text: heading) }
+      it { should have_title(full_title(page_title)) }
     end
-    it "should have the content 'Contact information'" do
-      visit contact_path
-      expect(page).to have_content('Contact information')
+
+    describe "Home page" do
+      before { visit root_path }
+      let(:heading)    { 'Activity Page' }
+      let(:page_title) { '' }
+
+      it_should_behave_like "all static pages"
+      it { should_not have_title('| Home') }
     end
-  end
+
+    describe "About page" do
+      before { visit about_path }
+
+      let(:heading)    { 'About' }
+      let(:page_title) { 'About' }
+
+      it_should_behave_like "all static pages"
+    end
+
+    describe "Contact page" do
+      before { visit contact_path }
+
+      let(:heading)    { 'Contact' }
+      let(:page_title) { 'Contact' }
+
+      it_should_behave_like "all static pages"
+    end
+
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_title(full_title('About Me'))
+      click_link "Contact"
+      expect(page).to have_title(full_title('Contact'))
+      click_link "Home"
+      click_link "Sign up now!"
+      expect(page).to have_title(full_title('Registration'))
+    end
 
 end
