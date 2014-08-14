42c7c1e83fde56619a361cd0643ff2c59c286e22
Ricardo Canales <canalesb93@gmail.com>
2014-08-13 22:59:50 -0500

    First user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #success
+      flash[:success] = "Welcome to my Activity Page!"
+      redirect_to @user
     else
       render 'new'
     end
