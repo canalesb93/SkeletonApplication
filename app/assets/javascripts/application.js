7b66678846dd5dfd9396c17540b143a9fe4ee9ff
Ricardo Canales <canalesb93@gmail.com>
2014-08-15 23:57:51 -0500

    App js improvement, +padding fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,21 +21,33 @@
 //= require turbolinks
 $(function(){
   $('.btn-toggle').click(function() {
+    $(this).find('.btn').toggleClass('active');  
+    
+    if ($(this).find('.btn-primary').size()>0) {
+      $(this).find('.btn').toggleClass('btn-primary');
+    }
+    if ($(this).find('.btn-danger').size()>0) {
+      $(this).find('.btn').toggleClass('btn-danger');
+    }
+    if ($(this).find('.btn-success').size()>0) {
+      $(this).find('.btn').toggleClass('btn-success');
+    }
+    if ($(this).find('.btn-info').size()>0) {
+      $(this).find('.btn').toggleClass('btn-info');
+    }
+    
+    $(this).find('.btn').toggleClass('btn-default');
+  })
+
+  $('#navbar').click(function() {
     $('#navbar').toggleClass('navbar-fixed-top')
     $('#navbar').toggleClass('navbar-static-top')
 
     if($('#navbar').hasClass('navbar-fixed-top')){
-      $( 'body' ).css( "padding-top", "65px" );
+      $( 'body' ).css( "padding-top", "71px" );
     } else {
       $( 'body' ).css( "padding-top", "0px" );
     }
-
-    $('#off-btn').toggleClass('active');
-    $('#off-btn').toggleClass('btn-default');
-    $('#off-btn').toggleClass('btn-primary');
-    $('#on-btn').toggleClass('active');
-    $('#on-btn').toggleClass('btn-default');
-    $('#on-btn').toggleClass('btn-primary');
-    
   })
+
 });
