55f6c6b3864ba9b81b6ccc90355d045e1db00bd4
Ricardo Canales <canalesb93@gmail.com>
2014-08-16 20:43:52 -0500

    Authentication process, holy fuck tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -20,12 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    
-    @user = User.find(params[:id])
     if @user.authenticate(user_params[:password]) != false
       if @user.update_attributes(user_params)
         flash[:success] = "Profile updated"
@@ -44,6 +43,20 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  #Before filters
+
+  def signed_in_user
+    unless signed_in?
+      flash[:warning] = "Please sign in"
+      redirect_to signin_url 
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,8 @@
 include ApplicationHelper
 
-def sign_in(user, options = {})
+def sign_in(user, options={})
   if options[:no_capybara]
-    #Sign in when not using capybara
+    # Sign in when not using Capybara.
     remember_token = User.new_remember_token
     cookies[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.digest(remember_token))
